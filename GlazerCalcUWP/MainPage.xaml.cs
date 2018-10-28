634e41c422232c5a142133f56afaf24d409307e0
minzoe <min16009@byui.edu>
2018-10-27 18:58:09 -0600

    Finished

--- GlazerCalcUWP/MainPage.xaml.cs
+++ GlazerCalcUWP/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,71 @@ namespace GlazerCalcUWP
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            Slider slide = sender as Slider;
+            txtQuanity.Text = slide.Value.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            widthString = widthInput.Text;
+            width = double.Parse(widthString);
+            heightString = heightInput.Text;
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            lengthOutput.Text = woodLength.ToString();
+            areaOutput.Text = glassArea.ToString();
+        }
+
+        private void widthInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string value = widthInput.Text;
+            double number;
+            if (Double.TryParse(value, out number))
+            {
+                if (number > 0)
+                {
+                    widthInput.BorderBrush = new SolidColorBrush(Colors.Gray);
+                }
+                else
+                {
+                    widthInput.BorderBrush = new SolidColorBrush(Colors.Red);
+                }
+            }
+            else
+            {
+                widthInput.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+        }
+
+        private void heightInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string value = heightInput.Text;
+            double number;
+            if (Double.TryParse(value, out number))
+            {
+                if (number > 0)
+                {
+                    heightInput.BorderBrush = new SolidColorBrush(Colors.Gray);
+                }
+                else
+                {
+                    heightInput.BorderBrush = new SolidColorBrush(Colors.Red);
+                }
+            }
+            else
+            {
+                heightInput.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
         }
     }
 }
